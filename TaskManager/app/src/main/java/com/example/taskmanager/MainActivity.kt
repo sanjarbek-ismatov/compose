f12dfa303792aa6b0f2fb6afc54bc4c63cdf7a1d
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-12-03 19:36:58 +0500

    Task manager task completed

--- TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Container()
                 }
             }
         }
@@ -30,17 +42,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Container(modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        CheckedImage()
+        TextBox()
+    }
+}
+
+@Composable
+fun CheckedImage(modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.ic_task_completed)
+    Image(
+        painter = image,
+        contentDescription = stringResource(R.string.task_image),
         modifier = modifier
+            .width(250.dp)
+            .height(250.dp)
+    )
+}
+
+@Composable
+fun TextBox(modifier: Modifier = Modifier){
+    Text(
+        text = stringResource(R.string.all_tasks_completed),
+        fontWeight = FontWeight.Bold,
+        modifier = modifier.padding(
+            top = 24.dp,
+            bottom = 8.dp
+        )
+    )
+    Text(
+        text = stringResource(R.string.nice_work),
+        fontSize = 16.sp
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContainerPreview(){
     TaskManagerTheme {
-        Greeting("Android")
+        Container()
     }
 }
+
